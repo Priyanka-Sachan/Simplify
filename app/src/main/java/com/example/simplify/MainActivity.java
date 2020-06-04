46b93eaaecf70ee9d1f38b3e72f608cd33324184
Priyanka Sachan <56231576+Priyanka-Sachan@users.noreply.github.com>
2020-06-04 19:46:36 +0530

    Add recycler view and adapter.

--- app/src/main/java/com/example/simplify/MainActivity.java
+++ app/src/main/java/com/example/simplify/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -18,12 +20,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView=findViewById(R.id.recycler_notes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final NoteAdapter noteAdapter=new NoteAdapter();
+        recyclerView.setAdapter(noteAdapter);
         //ViewModelProviders is now deprecated.
         noteViewModel= new ViewModelProvider(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<NoteClass>>() {
             @Override
             public void onChanged(List<NoteClass> noteClasses) {
-                Toast.makeText(getApplicationContext(),"Updated",Toast.LENGTH_SHORT).show();
+                noteAdapter.setNotes(noteClasses);
             }
         });
     }
---
+++ app/src/main/java/com/example/simplify/NoteAdapter.java
@@ -0,0 +1,54 @@
+package com.example.simplify;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+    List<NoteClass> notes= new ArrayList<NoteClass>();
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_note, parent, false);
+        return new NoteViewHolder(itemView);    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        NoteClass currentNote = notes.get(position);
+        holder.title.setText(currentNote.getTitle());
+        holder.content.setText(currentNote.getContent());
+        holder.priority.setText(String.valueOf(currentNote.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<NoteClass> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder{
+        TextView priority;
+        TextView title;
+        TextView content;
+
+        public NoteViewHolder(View itemView){
+            super(itemView);
+            priority=itemView.findViewById(R.id.priority);
+            title=itemView.findViewById(R.id.title);
+            content=itemView.findViewById(R.id.content);
+        }
+    }
+}
